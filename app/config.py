2ac3805f56d32d180ed7cddf4947c282c82cb2ac
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-05-23 11:52:37 -0500

    configuration end

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mssql+pyodbc://username:password@server/database?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mssql+pyodbc://DESKTOP-3KMPQST\\PRGWEB2:@DESKTOP-3KMPQST/items?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
